862a27b0abac767bf0980397cc3dbd72729e3f35
SalamaNaief <eng.salama.naief@gmail.com>
2023-06-10 07:01:10 +0300

    dd

--- paymentsProdviders/paytabs.js
+++ paymentsProdviders/paytabs.js
@@ -1,6 +1,7 @@
 import paytabs from "paytabs_pt2";
 import cartModel from "../models/cart.model.js";
 import { NotFoundError } from "../errors/index.js";
+import userModel from "../models/user.model.js";
 
 export const PayWithPaytabs = async (req, res) => {
   //paytabs configrations
@@ -103,6 +104,36 @@ export const paytabsWebhooks = async (req, res) => {
   const sig = req.headers["signature"];
   console.log("Signature", sig);
   console.log("Signature body", req.body);
-  // paytabs.validatePayment();
+
+  async function verifyPayment(result) {
+    if (result["response_code:"] === 400) {
+      console.log("false");
+      return res
+        .status(400)
+        .json({ message: "something went wrong in the payment!" });
+    } else {
+      const cart = await cartModel.findById(req.body.cart_id);
+      if (!cart) {
+        return res.status(404).json({ message: "cart is not found!" });
+      }
+      const user = await userModel.findOne({
+        email: req.body.customer_details.email,
+      });
+      if (!user) {
+        return res.status(404).json({ message: "user is not found!" });
+      }
+      const shipping_address = {
+        details: req.body.customer_details.street1,
+        city: req.body.customer_details.city,
+        postalCode: req.body.customer_details.zip,
+        phone: req.body.customer_details.phone,
+      };
+      console.log("true");
+      console.log("cart", cart);
+      console.log("cart", cart);
+      console.log("shipping_address", shipping_address);
+    }
+  }
+  paytabs.validatePayment(req.body.tran_ref, verifyPayment);
   res.send("");
 };
